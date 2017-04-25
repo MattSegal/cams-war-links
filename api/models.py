3ad016b47cbb08d717596cd5ecca0d7fc28ca613
MattSegal <mattdsegal@gmail.com>
2017-04-25 21:53:12 +1000

    Add tags to server side

--- api/models.py
+++ api/models.py
@@ -4,21 +4,28 @@ from django.contrib.auth.models import User
 # see https://docs.djangoproject.com/en/1.10/ref/contrib/auth/#django.contrib.auth.models.User
 # for user API
 
+class Tag(models.Model):
+    """ A tag belonging to a link
+    """
+    name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.name
+
 class Link(models.Model):
     """ A hyperlink belonging to a user
     """
     id              = models.AutoField(primary_key=True)
     user            = models.ForeignKey(User, related_name='links')
     bookmarkers     = models.ManyToManyField(User, related_name='bookmarks')
-    title           = models.CharField(max_length=250) # Unsure of max_length
-    url             = models.CharField(max_length=250) # Unsure of max_length
+    tags            = models.ManyToManyField(Tag, related_name='tags')
+    title           = models.CharField(max_length=250)
+    url             = models.CharField(max_length=250)
     description     = models.TextField(default='', blank=True)
     created         = models.DateTimeField(auto_now_add=True)
     modified        = models.DateTimeField(auto_now=True)
     active          = models.BooleanField(default=True)
 
-    def __repr__(self):
-        return '<Link: {0} - {1}>'.format(self.user,self.title)
+    def __unicode__(self):
+        return u'{0} - {1}'.format(self.user,self.title)
 
-    def __str__(self):
-        return self.__repr__()
--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Link
+from .models import Link, Tag
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id','username','bookmarks',)
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('name',)
+
 class LinkSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     created = serializers.DateTimeField(read_only=True)
+    tags = serializers.SerializerMethodField()
     
     class Meta:
         model = Link
-        fields = ('id','user','title','url','created','description',)
+        fields = ('id','user','title','url','created','description','tags',)
+
+    def get_tags(self, obj):
+        return [tag for tag in TagSerializer(obj.tags).data]
