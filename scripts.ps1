24b94c4b0b0a40ec3b0b0cd1a42a58eb94b3ee02
MattSegal <mattdsegal@gmail.com>
2017-10-13 11:04:08 +1100

    add dev script

---
+++ scripts.ps1
@@ -0,0 +1,46 @@
+param([String]$Option)
+
+function ActivateEnv
+{
+    $env:ALLOWED_HOSTS = "localhost,192.168.1.9"
+    .\env\Scripts\activate
+}
+
+function RunDjango 
+{
+    ActivateEnv
+    py .\manage.py runserver 0.0.0.0:80
+}
+
+function RunWebpack 
+{
+    Get-Item .\assets\*.css | % {Remove-Item $_.FullName}
+    Get-Item .\assets\*.js  | % {Remove-Item $_.FullName}
+    npm run dev
+}
+
+
+function RunRedis 
+{
+    redis-server
+}
+
+function RunDevEnvironment
+{
+    $dir = pwd
+    powershell -NoExit -new_console:f:n:s75V:t:"Webpack" "cd $dir;. ./scripts.ps1;RunWebpack"
+    powershell -NoExit -new_console:f:n:s66V:t:"Django" "cd $dir;. ./scripts.ps1;RunDjango" 
+    # powershell -NoExit -new_console:f:n:s33V:t:"Redis" "cd $dir;. ./scripts.ps1;RunRedis"  
+    ActivateEnv
+}
+
+switch ($Option)
+{
+    'dev' {
+        RunDevEnvironment
+    }
+    Default {
+        Write-Host "`n===== Links Scripts ====="
+        Write-Host "dev   Run dev environment"
+    }
+}
